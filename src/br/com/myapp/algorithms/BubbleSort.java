112d2ddb50d8f3fdef4f5528776842821bcd7ec2
Tiago <tiagohirute@gmail.com>
2024-05-25 00:45:55 -0300

    initial commit

---
+++ src/br/com/myapp/algorithms/BubbleSort.java
@@ -0,0 +1,40 @@
+package br.com.myapp.algorithms;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		
+		//inicialização do array
+		int[] numbers = {64, 34, 25, 12, 22, 11, 90};
+		
+		//for para o comprimento do array
+		for(int i = 0; i < numbers.length - 1; i++) {
+			
+			//
+			for(int j = 0; j < numbers.length - i - 1; j++) {
+				
+				if(numbers[j] > numbers[j + 1]) {
+					
+					int temp = numbers[j];
+					numbers[j] = numbers[j + 1];
+					numbers[j + 1] = temp;
+					
+				}
+				
+			}
+			
+		}
+		
+		System.out.println("Array ordenada: ");
+		
+		for(int i = 0; i < numbers.length; i++) {
+			
+			System.out.println(numbers[i] + " ");
+			
+		}
+		
+		System.out.println();
+		
+	}
+	
+}
