a7511f9a0ad56502c75d55ade5633d1fa4611ccd
Tiago <tiagohirute@gmail.com>
2024-05-25 20:40:18 -0300

    insertionsort added

--- src/br/com/myapp/algorithms/InsertionSort.java
+++ src/br/com/myapp/algorithms/InsertionSort.java
@@ -3,8 +3,46 @@ package br.com.myapp.algorithms;
 public class InsertionSort {
 
 	public static void main(String[] args) {
+
+		int vetor[] = new int[10];
 		
-		//
+		for(int i = 0; i < vetor.length; i++) {
+			
+			vetor[i] = (int) Math.floor(Math.random() * vetor.length);
+			
+		}
+		
+		System.out.println("Desordenado");
+		for(int i = 0; i < vetor.length; i++) {
+			
+			System.out.println(vetor[i] + " ");
+			
+		}
+		
+		int aux, j;
+		
+		for(int i = 1; i < vetor.length; i++) {
+			
+			aux = vetor[i];
+			
+			j = i - 1;
+			
+			while(j >=  0 && vetor[j] > aux) {
+				
+				vetor[j+1] = vetor[j];
+				j--;
+				
+			}
+			vetor[j+1 ] = aux;
+					
+		}
+		
+		System.out.println("\n\nOrdenado");
+		for(int i = 0; i < vetor.length; i++) {
+			
+			System.out.println(vetor[i] + " ");
+			
+		}
 		
 	}
 	
